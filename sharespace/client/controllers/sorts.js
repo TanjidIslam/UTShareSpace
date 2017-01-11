2a9ce81f8bcc03af72e113b9854674522bd52c0d
anastasiosexacoustos1993 <anastasios.exacoustos@gmail.com>
2017-01-11 09:39:26 -0500

    - Users are now able to sort based on when a posts were added, when posts were updated and how many votes posts have - Added timestamps in order to use them on sorting - Made posts and posts_by_tag more interactive - Changed Go Back href’s to what the previous link was in cpu history - Added controller that handles sorting functionalities

---
+++ sharespace/client/controllers/sorts.js
@@ -0,0 +1,48 @@
+// Application object
+var myApp = angular.module('myApp');
+
+// Name of the controller and:
+// 1. $scope is what binds controller to the view (pass things back and forth)
+myApp.controller('sort_controller', ['$scope', function($scope){
+
+	// Set initial value of sorting property
+	$scope.sort_property = "createdAt";
+
+	// Set initial value of sorting status
+	$scope.sort_status = "false";
+
+	// Trigger the function sort_button
+	$scope.sort_button = function(propertyName){
+
+		// Case where user selects to sort based on date that posts were added
+		if (propertyName == "createdAt") {
+
+			// Sort based on date added
+			$scope.sort_property = "createdAt";
+			// Set status to false
+			$scope.sort_status = "false";
+
+		// Case where user selects to sort based on date that posts were updated
+		} else if (propertyName == "updatedAt") {
+
+			// Sort based on date updated
+			$scope.sort_property = "updatedAt";
+			// Set status to false
+			$scope.sort_status = "false";
+
+		// Case where user selects to sort based on the amount of votes
+		} else if (propertyName == "votes") {
+
+			// Sort based on votes
+			$scope.sort_property = "votes";
+			// Sert status to true
+			$scope.sort_status = "true";
+
+		// Case where user selects to sort based on trends
+		} else {
+
+			// Print a simple statement mentioning "To be completed"
+			console.log("To be completed.")
+		}
+    }
+}]);
